486179227755647277c2a3e3cc63c9f0809dd76a
Parth <psharma66846@gmail.com>
2025-07-14 23:21:35 +0530

    Moved Firebase config to .env for security

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAuu9XatsNaezT7PmgAcVx24ZDLxymrHZs",
-    authDomain: "invoice-generator-52e5e.firebaseapp.com",
-    projectId: "invoice-generator-52e5e",
-    storageBucket: "invoice-generator-52e5e.firebasestorage.app",
-    messagingSenderId: "629321061333",
-    appId: "1:629321061333:web:724298520463efd33f235c",
-    measurementId: "G-2D2YX3LXR4"
-};
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
